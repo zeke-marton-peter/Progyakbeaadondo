883fe4162763b062880ca773dbe72a22edf48f3e
zeke-marton-peter <60149640+zeke-marton-peter@users.noreply.github.com>
2020-01-21 20:37:13 +0100

    Add files via upload
    Konstruktor öröklés majd rendezés

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgGyakBeadando
+{
+    public class Gamma
+    {
+        public static int randNum;
+        public Gamma()
+        {
+            Random rand = new Random();
+            randNum = rand.Next(1, 100);
+        }
+    }
+    public class Beta : Program 
+    {
+        public static void Rendezés()
+        {
+            for (int j = th - 1; j > 0; j--)
+            {
+                for (int k = 0; k < j; k++)
+                {
+                    if (tömbz[k] > tömbz[k + 1])
+                    {
+                        int s = tömbz[k + 1];
+                        tömbz[k + 1] = tömbz[k];
+                        tömbz[k] = s;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProgGyakBeadando
+{
+    public class Program
+    {
+        public static int[] tömbz;
+        public static int th;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mennyi legyen a tömb hossza?");
+            th = Convert.ToInt32(Console.ReadLine());
+            tömbz = new int[th];
+            for (int i = 0; i < tömbz.Length; i++)
+            {
+                _ = new Gamma();
+                tömbz[i] = Gamma.randNum;
+            }
+            
+            Console.WriteLine("Még nem rendezett tömb:");
+            for (int i = 0; i < th; i++)
+            {
+                Console.Write(tömbz[i] + " ");
+            }
+            Console.WriteLine();
+            Beta.Rendezés();
+            Console.WriteLine("Már rendezett tömb:");
+            for (int i = 0; i < th; i++)
+            {
+                Console.Write(tömbz[i] + " ");
+
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
